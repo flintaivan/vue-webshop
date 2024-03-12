b6e261b7fb87d6eec5c17fec437ad1b6af93c6a7
flintaivan <flinta.ivan@gmail.com>
2024-03-12 13:10:01 +0100

    Remove container

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
   <div>
     <HeaderComponent />
-    <div class="container-xl max-w-screen-xl mx-auto p-4">
       <router-view></router-view>
-    </div>
   </div>
 </template>
 
 <script>
 // Components
-import HeaderComponent from './components/Header/HeaderComponent.vue';
-
+import HeaderComponent from './components/Header/HeaderComponent.vue'
 export default {
   components: {
     HeaderComponent,
