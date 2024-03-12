70765f9d187cdec56c9cf85a820535493165a7f1
flintaivan <flinta.ivan@gmail.com>
2024-03-12 17:13:55 +0100

    Refactor Navbar component

--- src/components/Header/NavbarComponent.vue
+++ src/components/Header/NavbarComponent.vue
@@ -5,34 +5,22 @@
                 <Icon icon="simple-icons:logitech" class="h-14 w-14" />
             </div>
             <div class="flex space-x-6">
-                <router-link to="/" class="flex items-start px-4 py-3 space-x-2 transition-all duration-150 ease-linear hover:bg-indigo-900 rounded">
-                    <Icon icon="typcn:home" class="h-5 w-5" />
-                    <span>Home</span>
-                </router-link>
-                <router-link to="/about" class="flex items-start px-4 py-3 space-x-2 transition-all duration-150 ease-linear hover:bg-indigo-900 rounded">
-                    <Icon icon="mdi:eye" class="h-5 w-5" />
-                    <span>About</span>
-                </router-link>
-                <router-link to="/product" class="flex items-start px-4 py-3 space-x-2 transition-all duration-150 ease-linear hover:bg-indigo-900 rounded">
-                    <Icon icon="ri:box-3-fill" class="h-5 w-5" />
-                    <span>Products</span>
-                </router-link>
-                <router-link to="/contact" class="flex items-start px-4 py-3 space-x-2 transition-all duration-150 ease-linear hover:bg-indigo-900 rounded">
-                    <Icon icon="ic:baseline-mail" class="h-5 w-5" />
-                    <span>Contact</span>
-                </router-link>
+                <NavbarLinkComponent />
             </div>
         </div>
     </div>
 </template>
 
 <script>
-// Icons 
+// Components 
+import NavbarLinkComponent from './NavbarLinkComponent.vue'
+// Icons
 import { Icon } from '@iconify/vue';
     export default {
         name: 'NavbarComponent',
         components: {
-            Icon
+            Icon,
+            NavbarLinkComponent,
         }
     }
 </script>
