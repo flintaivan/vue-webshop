7d579d027cd542c970716d86b2bec69599d339ae
flintaivan <flinta.ivan@gmail.com>
2024-03-12 17:13:09 +0100

    Style Footer component

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div>
     <HeaderComponent />
       <router-view></router-view>
+      <FooterComponent />
   </div>
 </template>
 
 <script>
 // Components
 import HeaderComponent from './components/Header/HeaderComponent.vue'
+import FooterComponent from './components/Footer/FooterComponent.vue';
 export default {
   components: {
     HeaderComponent,
+    FooterComponent
   }
 }
 </script>
--- src/components/Footer/FooterComponent.vue
+++ src/components/Footer/FooterComponent.vue
@@ -1,13 +1,34 @@
 <template>
-    <div>
+    <div class="bg-gray-800 text-indigo-50 h-auto">
+        <div class="container-xl max-w-screen-xl mx-auto py-20 px-4 flex justify-between items-center">
+            <div class="flex">
+                <NavbarLinkComponent />
+            </div>
+            <div class="flex space-x-4">
+                <div class="p-2">
+                    <Icon icon="ic:round-facebook" class="w-6 h-6 text-indigo-50 hover:text-indigo-300 cursor-pointer" />
+                </div>
+                <div class="p-2">
+                    <Icon icon="ri:instagram-fill" class="w-6 h-6 text-indigo-50 hover:text-indigo-300 cursor-pointer" />
+                </div>
 
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        
+// Components
+import NavbarLinkComponent from '../Header/NavbarLinkComponent.vue'
+// Icons
+import { Icon } from '@iconify/vue';   
+export default {
+    name: 'FooterComponent',
+    components: {
+        NavbarLinkComponent,
+        Icon,
     }
+}
 </script>
 
 <style lang="scss" scoped>
