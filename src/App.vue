3de28b7f245509b41acde2f6c832649ccb7ba87c
flintaivan <flinta.ivan@gmail.com>
2024-03-13 16:47:59 +0100

    Make HeaderComponent reusable

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
-    <HeaderComponent />
+    <HeaderComponent :isHomepage="isHomepage()" />
       <router-view></router-view>
       <FooterComponent />
   </div>
 </template>
 
 <script>
+// Vue core
+import { useRoute } from 'vue-router';
+import { watch } from 'vue';
 // Components
 import HeaderComponent from './components/Header/HeaderComponent.vue'
 import FooterComponent from './components/Footer/FooterComponent.vue';
@@ -14,7 +17,32 @@ export default {
   components: {
     HeaderComponent,
     FooterComponent
-  }
+  },
+  setup() {
+    const route = useRoute()
+    const isHomepage = () => {
+      let data
+      if(route.name == 'Home') {
+        data = {
+          height: 'h-[700px]',
+          title: 'Welcome to Logi online store',
+          subtitle: 'This is some subtitle message here',
+          button: true
+          } 
+      } else {
+        data = {
+          height: 'h-[300px]',
+          title: 'Check out all products',
+          subtitle: 'This is some subtitle message here',
+          button: false
+          } 
+        }
+        return data
+      } 
+    return {
+      isHomepage,
+    }
+  },
 }
 </script>
 
--- src/components/Header/HeaderComponent.vue
+++ src/components/Header/HeaderComponent.vue
@@ -1,11 +1,13 @@
 <template>
     <div>
         <NavbarComponent />
-        <div class="max-w-screen h-[700px] bg-header-image bg-center flex items-center justify-center">
+        <div 
+        :class="isHomepage.height"
+        class="max-w-screen bg-header-image bg-center flex items-center justify-center">
             <div class="container-xl max-w-screen-xl mx-auto text-indigo-100 flex flex-col items-center space-y-6">
-                <h1 class="text-6xl drop-shadow-xl">Welcome to Logi online store</h1>
-                <span class="text-xl">This is some subtitle message here</span>
-                <button class="btn btn-primary">Check out products</button>
+                <h1 class="text-6xl drop-shadow-xl">{{ isHomepage.title }}</h1>
+                <span class="text-xl">{{ isHomepage.subtitle }}</span>
+                <button v-if="isHomepage.button" class="btn btn-primary">Check out products</button>
             </div>
         </div>
     </div>
@@ -18,8 +20,13 @@ export default {
     name: 'HeaderComponent',
     components: {
         NavbarComponent,
+    },
+    props: {
+        isHomepage: {
+            type: Object,
+            required: true
+        }
     }
-    
 }
 </script>
 
