2a0ca5a5f98e843b5005c9fbd6792f0b19fa2098
flintaivan <flinta.ivan@gmail.com>
2024-03-14 13:48:10 +0100

    Install primevue icons, add animatecss cdn

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import 'primeicons/primeicons.css';
+import 'primevue/resources/themes/saga-blue/theme.css';
 
 
 const pinia = createPinia()
